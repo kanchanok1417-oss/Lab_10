cad7f7d681f27311e25de8b34d401fd4855449ff
Kanchanok <kanchanok1417@gmail.com>
2025-12-24 23:13:00 +0700

    Add files via upload

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,18 +6,19 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
 	string textline;
     cout << "-------------------- BOOM ---------------------\n";
-
+	dest << "-------------------- BOOM ---------------------\n";
     while (getline(source, textline)){
         cout << textline << "\n";
+		dest << textline << endl;
     }
     
     cout << "-------------------- HA!! ---------------------\n";
-
+    dest << "-------------------- BOOM ---------------------\n";
     source.close();
     dest.close();
 	return 0;
